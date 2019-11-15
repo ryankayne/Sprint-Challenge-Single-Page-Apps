154ecda32202d81de90b11f5b5bf619a99a64303
ryankayne <coope1rk@gmail.com>
2019-11-15 12:20:07 -0500

    Got two pages working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage.js";
 
 export default function App() {
   return (
-    <main>
-      <Header />
-      <CharacterList />
+<main>
+  <Header />
+    <nav>
+        <div className="nav-links">
+          <Link className="home-link" to="/">Home</Link>
+          <Link className="char-link" to="/character-list">Characters</Link>
+
+          <Route exact path="/" component={WelcomePage} />
+          <Route exact path="/character-list" component={CharacterList} />
+        </div>
+        </nav>
+    
+      
+      
+      
     </main>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CharacterList from "./CharacterList";
+
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
+        
         <h1>Welcome to the ultimate fan site!</h1>
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
+        
       </header>
     </section>
   );
